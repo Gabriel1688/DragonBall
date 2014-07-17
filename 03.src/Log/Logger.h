608b7a29d71aa685867a58a0e97784864eaa2210
james Huang <james@quixotiq.mobi>
2014-07-17 19:59:20 -0400

    uml changed

--- 03.src/Log/Logger.h
+++ 03.src/Log/Logger.h
@@ -7,14 +7,32 @@
 
 #ifndef LOGGER_H_
 #define LOGGER_H_
+#include <string>
+#include <fstream>
+#include <iostream>
 
 class Logger {
 public:
+	 enum Priority
+	    {
+	        DEBUG,
+	        CONFIG,
+	        INFO,
+	        WARNING,
+	        ERROR
+	    };
+
+	static void Start(int priority,const string& logFile);
+	static void stop();
+	static void write(Priority priority,const string& message)
+	virtual ~Logger();
+private:
 	Logger();
 	Logger(Logger &logger);
-	Logger&  operator = (Logger &logger);
-	bool Start(int priority,string &message);
-	virtual ~Logger();
+	Logger&  operator = (Logger &logger) {}
+
+	static Logger instance;
+
 };
 
 #endif /* LOGGER_H_ */
